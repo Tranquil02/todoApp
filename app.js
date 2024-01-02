22b5758576c076256967f96af4baf1d73633e15b
Prashant Kumar Rajak <106570525+Tranquil02@users.noreply.github.com>
2024-01-02 20:21:33 +0530

    cors updated

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ export const app=express();
 config({
     path:"./data/config.env"
 })
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
+    next();
+  });
 
 // Using MiddleWare
 app.use(express.json());
