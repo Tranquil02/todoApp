b791f67fc4ed1bc186b731ba0f3ffa708a3ec485
Prashant <prashantkashyap2707@gmail.com>
2024-01-07 09:25:43 +0530

    commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ config({
     path:"./data/config.env"
 })
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
